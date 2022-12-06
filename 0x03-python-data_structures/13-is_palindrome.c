bce2f32c71ae32b087dad44f5a976d979353bf64
obaraelijah <elijahobara357@gmail.com>
2022-12-06 06:12:14 +0000

    Hot fix

--- 0x03-python-data_structures/13-is_palindrome.c
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -1,40 +1,62 @@
-#include "lists.h"
-#include <string.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include "lists.h"
 
+typedef struct listint {
+  int n;
+  struct listint *next;
+} listint_t;
 /**
   * is_palindrome - A function that checks if a list is a palindrome.
   * @head: The pointer to the head of the list.
   * Return: 0 if list not a palindrome, 1 if it is.
   */
-int is_palindrome(listint_t **head)
-{
-	listint_t *tmp = *head;
-	int nodes = 0, i = 0, *array = NULL;
 
-	if (*head == NULL || head == NULL || (*head)->next == NULL)
-		return (1);
-	while (tmp)
-	{
-		nodes++;
-		tmp = tmp->next;
-	}
-	array = malloc(sizeof(int) * nodes);
-	tmp = *head;
-	while (tmp)
-	{
-		array[i++] = tmp->n;
-		tmp = tmp->next;
-	}
-	for (i = 0; i < nodes / 2; i++)
-	{
-		if (array[i] != array[nodes - 1 - i])
-		{
-			free(array);
-			return (0);
-		}
-	}
-	free(array);
-	return (1);
+int is_palindrome(listint_t **head) {
+  // Handle empty list
+  if (*head == NULL) {
+    return 1;
+  }
+
+  // Find the middle of the list
+  listint_t *slow = *head;
+  listint_t *fast = *head;
+  while (fast != NULL && fast->next != NULL) {
+    slow = slow->next;
+    fast = fast->next->next;
+  }
+
+  // Reverse the second half of the list
+  listint_t *prev = NULL;
+  listint_t *curr = slow;
+  while (curr != NULL) {
+    listint_t *next = curr->next;
+    curr->next = prev;
+    prev = curr;
+    curr = next;
+  }
+
+  // Compare the first and second half of the list
+  listint_t *left = *head;
+  listint_t *right = prev;
+  while (right != NULL) {
+    if (left->n != right->n) {
+      return 0;
+    }
+    left = left->next;
+    right = right->next;
+  }
+
+  // Restore the original list
+  curr = prev;
+  prev = NULL;
+  while (curr != NULL) {
+    listint_t *next = curr->next;
+    curr->next = prev;
+    prev = curr;
+    curr = next;
+  }
+
+  return 1;
 }
+
