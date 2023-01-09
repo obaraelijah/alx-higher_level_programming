43462eaab07d5caacf25d65391c092748d06136b
obaraelijah <elijahobara357@gmail.com>
2023-01-09 07:51:22 +0000

    Task 7

---
+++ 0x0A-python-inheritance/7-base_geometry.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python3
+"""
+A integer validator module
+"""
+
+
+class BaseGeometry:
+    """
+    A super class to implements geometrical shapes
+    """
+
+    def area(self):
+        """
+        Raises an exception when you call this function
+        """
+
+        raise Exception('area() is not implemented')
+
+    def integer_validator(self, name, value):
+        """
+        checks a integer value
+        Args:
+            name (str): The name of the value.
+            value (int): The value.
+        Raises:
+            TypeError: If `value` isn't a integer.
+            ValueError: If `value` is less than or equal to zero.
+        """
+    
+
+        if type(value) is not int:
+            raise TypeError(name + ' must be an integer')
+
+        if value <= 0:
+            raise ValueError(name + ' must be greater than 0 ')
