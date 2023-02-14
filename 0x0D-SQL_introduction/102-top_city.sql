915430a9d02411d5d5833463d15e23e825d4075a
obaraelijah <elijahobara357@gmail.com>
2023-02-14 16:48:23 +0000

    Advanced task

--- 0x0D-SQL_introduction/102-top_city.sql
+++ 0x0D-SQL_introduction/102-top_city.sql
@@ -1,2 +1,4 @@
+-- Display highest temperatures
+-- DML query to list top 3 sorted temperatures during July and Agust
 SELECT city, AVG(value) AS avg_temp FROM temperatures WHERE month=7 OR month=8 GROUP BY city ORDER BY avg_temp DESC LIMIT 3;
 
