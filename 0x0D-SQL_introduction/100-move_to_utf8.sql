f283f910f8ce9043defe821841ec3a62d4fef4ce
obaraelijah <elijahobara357@gmail.com>
2023-02-14 16:42:07 +0000

    script that converts hbtn_0c_0 database to UTF8 (utf8mb4, collate utf8mb4_unicode_ci

---
+++ 0x0D-SQL_introduction/100-move_to_utf8.sql
@@ -0,0 +1,5 @@
+-- Converts encoding to UTF8
+-- DDL query to encoding hbtn_0c_0, first table, and name to UTF8
+ALTER DATABASE hbtn_0c_0 CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+USE hbtn_0c_0;
+ALTER TABLE first_table CONVERT TO CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
