2239030a8da798d1f92b2c627fd6c3219a351494
obaraelijah <elijahobara357@gmail.com>
2023-02-14 16:45:13 +0000

    Advanced task

---
+++ 0x0D-SQL_introduction/101-avg_temperatures.sql
@@ -0,0 +1,3 @@
+-- import temperatures into hbtn_0c_0 
+-- DML query to display average temperature by city
+SELECT city, AVG(value) AS avg_temp FROM temperatures GROUP BY city ORDER BY avg_temp DESC;
