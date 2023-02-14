cb6681c0f5f4ba101958b420efb16d8327f40e71
obaraelijah <elijahobara357@gmail.com>
2023-02-14 16:28:38 +0000

    script that lists the number of records with the same score in the table

---
+++ 0x0D-SQL_introduction/15-groups.sql
@@ -0,0 +1,3 @@
+-- Display same score using COUNT, ORDER BY, GROUP BY
+-- DML query to display number of records with same score
+SELECT score,COUNT(*) AS 'number' FROM second_table GROUP BY score ORDER BY DESC;
