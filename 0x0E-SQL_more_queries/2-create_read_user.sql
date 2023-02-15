e5ee9f81b8c2e00505d517cc5668025f6c1913d1
obaraelijah <elijahobara357@gmail.com>
2023-02-15 07:45:35 +0000

    Task three

---
+++ 0x0E-SQL_more_queries/2-create_read_user.sql
@@ -0,0 +1,8 @@
+-- Create a database and user
+-- DDL query to create an user with specific privileges
+CREATE DATABASE IF NOT EXISTS hbtn_0d_2;
+CREATE USER IF NOT EXISTS 'user_0d_2'@'localhost' IDENTIFIED BY 'user_0d_2_pwd';
+-- query privilegs for user_0d_2
+GRANT SELECT ON * TO 'user_0d_2'@'localhost';
+
+
