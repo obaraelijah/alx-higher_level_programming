1115c3d74fddba868c3b5501e0e08206fa640ca5
obaraelijah <elijahobara357@gmail.com>
2023-02-18 18:10:58 +0000

    Hot fix

--- 0x0E-SQL_more_queries/9-cities_by_state_join.sql
+++ 0x0E-SQL_more_queries/9-cities_by_state_join.sql
@@ -1,4 +1,4 @@
--- using join to display info about two tables
--- statement to do join query
+-- using join for  display info about two tables
+-- statement  join query
 SELECT cities.id, cities.name, states.name FROM cities INNER JOIN states ON cities.state_id=states.id ORDER BY cities.id ASC;
 
