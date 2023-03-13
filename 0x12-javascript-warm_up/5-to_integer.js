65530facef32ef35f31ee81b382ca6a60e62069e
obaraelijah <elijahobara357@gmail.com>
2023-03-13 15:26:39 +0000

    Hot fix

--- 0x12-javascript-warm_up/5-to_integer.js
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
 
 const arg = process.argv[2];
-const num = parseInt(arg); // converts  argument to n integer
+const num = parseInt(arg); //converts  argument to n integer
 
 if (isNaN(num)) {
   console.log('Not a number');
