249605f99f85aff0bebb0df2a9a5ea25b357de33
obaraelijah <elijahobara357@gmail.com>
2023-03-13 15:06:56 +0000

    Script that  prints two arguments passed to it in the specified format using console.log() without using var

--- 0x12-javascript-warm_up/4-concat.js
+++ 0x12-javascript-warm_up/4-concat.js
@@ -4,4 +4,7 @@
  * in the following format: “ is ”
  */
 
-console.log(process.argv[2] + 'is' + process.argv[3]);
+const arg1 = process.argv[2];
+const arg2 = process.argv[3];
+
+console.log(`${arg1} is ${arg2}`);
