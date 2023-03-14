7bd9340f84d8d307e307ba7bc2b36f5bab9c1993
obaraelijah <elijahobara357@gmail.com>
2023-03-14 04:57:04 +0000

    Function that executes n times a function

--- 0x12-javascript-warm_up/101-call_me_moby.js
+++ 0x12-javascript-warm_up/101-call_me_moby.js
@@ -1,1 +1,8 @@
 #!/usr/bin/node
+
+function callMeMoby (n, theFunction) {
+  for (let i = 0; i < n; i++) {
+    theFunction();
+  }
+}
+module.exports.callMeMoby = callMeMoby;
---
+++ 0x12-javascript-warm_up/101-main.js
@@ -0,0 +1,7 @@
+#!/usr/bin/node
+
+const callMeMoby = require('./101-call_me_moby').callMeMoby;
+
+callMeMoby(4, function() {
+ console.log('Sam is  Genius!');
+});
