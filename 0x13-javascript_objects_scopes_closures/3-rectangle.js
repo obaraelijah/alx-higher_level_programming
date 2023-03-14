d709777271150e836d7ad4e74b1e0e7213039587
obaraelijah <elijahobara357@gmail.com>
2023-03-14 13:50:39 +0000

    Hot fix

--- 0x13-javascript_objects_scopes_closures/3-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/3-rectangle.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
 
 module.exports = class Rectangle {
-  constructor (h, w) {
+  constructor (w, h) {
     if (w > 0 && h > 0) {
       this.width = w;
       this.height = h;
@@ -9,12 +9,8 @@ module.exports = class Rectangle {
   }
 
   print () {
-    let row = '';
-    for (let i = 0; i < this.width; i++) {
-      row += 'x';
-    }
-    for (let i = 0; i < this.height; i++) {
-      console.log(row);
-    }
+    let a = 'X'.repeat(this.width) + '\n';
+    a = a.repeat(this.height);
+    console.log(a.slice(0, -1));
   }
 };
