a788ce60e906de6db055f55906bdcc44bf30885f
obaraelijah <elijahobara357@gmail.com>
2023-03-14 19:56:25 +0000

    Advanced tasks files

--- 0x13-javascript_objects_scopes_closures/1-main.js
+++ 0x13-javascript_objects_scopes_closures/1-main.js
@@ -15,4 +15,3 @@ const r3 = new Rectangle(2);
 console.log(r3);
 console.log(r3.width);
 console.log(r3.height);
-
---
+++ 0x13-javascript_objects_scopes_closures/2-main.js
@@ -0,0 +1,7 @@
+#!/usr/bin/node
+const Rectangle = require('./2-rectangle');
+
+const r1 = new Rectangle(2, 3);
+console.log(r1);
+console.log(r1.width);
+console.log(r1.height);
