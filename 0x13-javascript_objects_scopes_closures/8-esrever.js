171496148c2bb57a9a0234a137f2e343752fc01d
obaraelijah <elijahobara357@gmail.com>
2023-03-15 07:25:08 +0000

    A function that reverses

--- 0x13-javascript_objects_scopes_closures/8-esrever.js
+++ 0x13-javascript_objects_scopes_closures/8-esrever.js
@@ -1,1 +1,10 @@
-#!/usr/bin/node
+#!/usr/bin/nod
+
+exports.esrever = function (list) {
+  const reversed = [];
+  for (let i = list.length - 1; i >= 0; i--) {
+    reversed.push(list[i]);
+  }
+
+  return reversed;
+};
