5908e6fb72f21047f76e9250c5017c492ab581f0
obaraelijah <elijahobara357@gmail.com>
2023-03-17 07:52:47 +0000

    Script safe from MySQL injections

--- 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -17,7 +17,7 @@ if __name__ == "__main__":
         print("error connecting")
     cur = connection.cursor()
     try:
-        cur.execute("SELECT * FROM states WHERE name = %s ORDER BY\
+        cur.execute("SELECT * FROM states WHERE name = ? ORDER BY\
         states.id", (sys.argv[4],))
         rows = cur.fetchall()
         for row in rows:
