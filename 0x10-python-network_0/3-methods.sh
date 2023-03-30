32bca3e2b8a38c5bf044bf4c419d316b9a40950c
obaraelijah <elijahobara357@gmail.com>
2023-03-30 16:18:05 +0000

    Hot fix leading space

--- 0x10-python-network_0/3-methods.sh
+++ 0x10-python-network_0/3-methods.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
 # Takes in a URL and displays all HTTP methods the server will accept
-
-curl -sI "$1" | grep "Allow" | cut -d ' ' -f 2- | sed 's/^[[:space:]]*//'
+curl -s -I -L -X OPTIONS "$1" | grep Allow | cut -d':' -f 2- | sed 's/^[[:space:]]*//'
