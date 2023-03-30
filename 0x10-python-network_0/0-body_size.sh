3a7a88ccdd602ef4a1c77d8a8cc5b280f5e6d2f4
obaraelijah <elijahobara357@gmail.com>
2023-03-30 15:54:04 +0000

    Bash script that takes in a URL, sends a request to that URL, and displays the size of the body of the response

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # Takes in a url and sends a request to that url and displays the size of the body of the response
-curl -s -w '%{size_uploa}\n' -o /dev/null "$1"
+curl -s -w '%{size_download}\n' -o /dev/null "$1"
