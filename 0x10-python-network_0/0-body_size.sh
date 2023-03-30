449913c4c895b6e384818c5c12b5d3455d543f47
obaraelijah <elijahobara357@gmail.com>
2023-03-30 15:47:34 +0000

    Bash script that takes in a URL, sends a request to that URL, and displays the size of the body of the response

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,4 +1,20 @@
 #!/bin/bash
-# Bash script that takes in a URL, sends a request to that URL, and displays the size of the body of the response
-curl -sI "$1" | grep "Content-Length" | awk '{print $2}'
+# This script takes a URL as an argument, sends a request to that URL using curl, and displays the size of the response body in bytes
 
+# Check if URL argument is provided
+if [ -z "$1" ]; then
+  echo "Error: Please provide a URL as an argument"
+  exit 1
+fi
+
+# Send curl request to URL and store response body in a variable
+response=$(curl -sI "$1" | grep -i Content-Length | awk '{print $2}')
+
+# Check if response is empty
+if [ -z "$response" ]; then
+  echo "Error: No response received from URL"
+  exit 1
+fi
+
+# Print the size of the response body in bytes
+echo "$response"
