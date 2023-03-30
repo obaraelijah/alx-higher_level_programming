0d88b41326efe1a858bd8077b2d1e3ccb5bf2469
obaraelijah <elijahobara357@gmail.com>
2023-03-30 16:02:27 +0000

    Script that sends a get request

--- 0x10-python-network_0/1-body.sh
+++ 0x10-python-network_0/1-body.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
-# Takes in a url and sends a GET Irequest to that url and displays the body of the response
-curl -s -L "$1"
+
+# Takes in a URL, sends a GET request to that URL, and displays the body of the response
+response=$(curl -s -o /dev/null -w "%{http_code}" "$1")  # Get the HTTP status code
+if [[ "$response" == "200" ]]; then  # If the status code is 200 OK
+  curl -s "$1"  # Display the body of the response
+fi
