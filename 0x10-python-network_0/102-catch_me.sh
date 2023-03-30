42ed5f85a78a0c90e92f4f4e97b7206a2010d826
obaraelijah <elijahobara357@gmail.com>
2023-03-30 19:26:47 +0000

    Added files

--- 0x10-python-network_0/102-catch_me.sh
+++ 0x10-python-network_0/102-catch_me.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 # makes a request to 0.0.0.0:5000/catch_me that causes the server to respond with "You got me!"
-curl -s -X PUT --data  0.0.0.0:5000/catch_me -H "Origin: HolbertonSchool"
+curl -s -X PUT --data "user_id=98" 0.0.0.0:5000/catch_me -H "Origin:HolbertonSchool"
 
