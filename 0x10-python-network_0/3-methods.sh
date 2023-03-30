4593b5c42e1571253e1392729ebee67e9e7f2ebb
obaraelijah <elijahobara357@gmail.com>
2023-03-30 16:16:36 +0000

    Bash script that takes in a URL as an argument, sends a GET request to the URL, and displays the body of the response

--- 0x10-python-network_0/3-methods.sh
+++ 0x10-python-network_0/3-methods.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 # Takes in a URL and displays all HTTP methods the server will accept
 
-curl -sI "$1" | grep "Allow" | cut -d ' ' -f 2-
+curl -sI "$1" | grep "Allow" | cut -d ' ' -f 2- | sed 's/^[[:space:]]*//'
