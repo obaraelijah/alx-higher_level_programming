596f59e02c85e627bc80571ce51b5e479aed3feb
obaraelijah <elijahobara357@gmail.com>
2023-03-30 19:03:19 +0000

    A script thata returns the status code

--- 0x10-python-network_0/100-status_code.sh
+++ 0x10-python-network_0/100-status_code.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
-# sends request to URL and capture status code
-
-curl -s  -w "%{response_code}" -o /dev/null "$1"
+# Sends a request to a URL and displays the status code
+curl -s -w "%{response_code}" -o /dev/null "$1"
