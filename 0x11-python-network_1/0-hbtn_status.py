1b6c0f1b82ec9ab088b4110b8949c79347063255
obaraelijah <elijahobara357@gmail.com>
2023-03-31 18:47:43 +0000

    A python script that fetches data from a given url

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,16 +1,12 @@
 #!/usr/bin/python3
-"""Fetches data from a url and displays in the body response using a urllib
-package
-"""
+"""Fetches https://alx-intranet.hbtn.io/status using urllib"""
 
-import urllib.request
-
-if __name__ == "main":
-    url="https://alx-intranet.hbtn.io/status"
-    with urllib.request.urlopen(url) as response:
+if __name__ == "__main__":
+    from urllib import request
+    url = 'https://alx-intranet.hbtn.io/status'
+    with request.urlopen(url) as response:
         data = response.read()
-        print('Body response')
-        print(f'\t- type:", {type(data)}')
+        print('Body response:')
+        print(f'\t- type: {type(data)}')
         print(f'\t- content: {data}')
         print(f'\t- utf8 content: {data.decode("UTF-8")}')
-
