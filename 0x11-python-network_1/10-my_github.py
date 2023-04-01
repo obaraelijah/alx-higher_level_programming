54fab7aff73a9a747eccadc6ea8147eae878ccb7
obaraelijah <elijahobara357@gmail.com>
2023-04-01 16:53:28 +0000

    Python script that takes your GitHub credentials (username and password) and uses the GitHub API to display your id

---
+++ 0x11-python-network_1/10-my_github.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""Python script that takes your GitHub credentials (username and password) and uses the GitHub API to display your id
+"""
+
+import sys
+import requests
+
+if __name__ == "__main__":
+    username = sys.argv[1]
+    passwd = sys.argv[2]
+
+    url = "https://api.github.com/user"
+
+    res = requests.get(url, auth=(username, passwd))
+    res = res.json()
+    print(res.get('id'))
