60bda8982df1a5a65856d40100c753e9be837818
obaraelijah <elijahobara357@gmail.com>
2023-04-01 16:11:46 +0000

    Python script that takes in a letter and sends a POST request

---
+++ 0x11-python-network_1/8-json_api.py
@@ -0,0 +1,27 @@
+#!usr/bin/python3
+"""Python script that takes in a letter and sends a POST request to http://0.0.0.0:5000/search_user with the letter as a parameter.
+"""
+import sys
+import requests
+
+if __name__ == "__main__":
+    a = {}
+    url = "http://0.0.0.0:5000/search_user"
+    if len(argv) < 2:
+        a["q"] = ""
+        else:
+        a["q"] = argv[1]
+
+        res = requests.post(url, data=a)
+
+        try:
+            data = res.json()
+        except ValueError:
+            print("Not a valid JSON")
+        else:
+            if data == {}:
+            print("Not a valid JSON")
+             else:
+                id = data.get('id')
+                name = data.get('name')
+                print(f"{id} {name}")
