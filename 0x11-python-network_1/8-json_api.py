fe0c01c5e0566a9e5849f87f377a1f010ae96db2
obaraelijah <elijahobara357@gmail.com>
2023-04-01 16:15:36 +0000

    Python script that takes in a letter and sends a POST request

--- 0x11-python-network_1/8-json_api.py
+++ 0x11-python-network_1/8-json_api.py
@@ -1,27 +1,29 @@
-#!usr/bin/python3
-"""Python script that takes in a letter and sends a POST request to http://0.0.0.0:5000/search_user with the letter as a parameter.
+#!/usr/bin/python3
+"""Takes in a letter and sends a POST request
+to http://0.0.0.0:5000/search_user with the letter as a parameter
 """
-import sys
-import requests
 
 if __name__ == "__main__":
-    a = {}
-    url = "http://0.0.0.0:5000/search_user"
-    if len(sys.argv) < 2:
-        a["q"] = ""
-        else:
-        a["q"] = sys.argv[1]
+    import requests
+    from sys import argv
 
-        res = requests.post(url, data=a)
+    d = {}
 
-        try:
-            data = res.json()
-        except ValueError:
-            print("Not a valid JSON")
+    if len(argv) < 2:
+        d["q"] = ""
+    else:
+        d["q"] = argv[1]
+
+    data = requests.post("http://0.0.0.0:5000/search_user", data=d)
+
+    try:
+        data = data.json()
+    except ValueError:
+        print("Not a valid JSON")
+    else:
+        if data == {}:
+            print("No result")
         else:
-            if data == {}:
-            print("Not a valid JSON")
-             else:
-                id = data.get('id')
-                name = data.get('name')
-                print(f"{id} {name}")
+            id = data.get('id')
+            name = data.get('name')
+            print(f"[{id}] {name}")
