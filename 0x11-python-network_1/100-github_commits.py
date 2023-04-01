5b2c103e17091aa9fd3988c3502f27efaa7a99a4
obaraelijah <elijahobara357@gmail.com>
2023-04-01 17:17:30 +0000

    Python script that takes two arguments (repository name and owner name) and uses the GitHub API to list the 10 most recent commits of that repository by the specified use

--- 0x11-python-network_1/100-github_commits.py
+++ 0x11-python-network_1/100-github_commits.py
@@ -1,17 +1,17 @@
 #!/usr/bin/python3
-
+"""Takes your github credentials and uses the github
+API to display your id
+"""
 import sys
 import requests
 
-if __name__ == "__main__":
-    
-    repo_name = sys.argv[1]
-    owner_name = sys.argv[2]
-
-    url = f"https://api.github.com/repos/{owner_name}/{repo_name}/commits"
-
+if __name__ == '__main__':
+ 
+    repo_owner = argv[1]
+    owner_name = argv[2]
+    url = f"https://api.github.com/repos/{owner_name}/{repo_owner}/commits"
     res = requests.get(url)
-
+    
     try:
         res = res.json()
     except ValueError:
