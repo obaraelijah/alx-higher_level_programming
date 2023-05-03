9b24660c4b058d5cfd998ec6159c0b1dc5619ce3
elijah obara <elijahobara357@gmail.com>
2023-05-03 21:50:53 +0300

    Hot fix

--- 0x15-javascript-web_jquery/8-script.js
+++ 0x15-javascript-web_jquery/8-script.js
@@ -1,2 +1,8 @@
 const url = 'https://swapi-api.hbtn.io/api/people/5/?format=json';
-$(document).ready()
+$(document).ready(() => {
+    $.get(url, function (data) {
+    $.each(data.results, function (i, movie) {
+      $('#list_movies').append('<li>' + movie.title + '</li>');
+    });
+});
+});
